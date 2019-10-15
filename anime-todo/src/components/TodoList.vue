81cf9da485e251d48686f99da703969f75ee208c
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2019-10-15 15:00:07 +0800

    Add a text box for user input and able to display anime titles in a list below

--- anime-todo/src/components/TodoList.vue
+++ anime-todo/src/components/TodoList.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    Todo list goes here
+    <input type="text" class="todo-input" placeholder="Watch an anime" v-model="newTodo">
+    <div v-for="todo in todos" :key="todo.id" class="todo-item">
+      {{ todo.title }}
+    </div>
   </div>
 </template>
 
@@ -9,12 +12,38 @@ export default {
   name: 'todo-list',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      newTodo: '',
+      todos: [
+        {
+          'id': 1,
+          'title': 'Yuru Yuri',
+          'completed': false,
+        },
+        {
+          'id': 2,
+          'title': 'Seitokai Yakuindomo',
+          'completed': false,
+        },
+        {
+          'id': 3,
+          'title': 'Joshiraku',
+          'completed': false,
+        }
+      ]
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+  .todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
 
+    &:focus {
+      outline: 0;
+    }
+  }
 </style>
