e2d45ae8a5eb4c51cd3e9bff429650b2927217b5
NotTheRealEdmund <edmund_teo_wei_xin@yahoo.com.sg>
2019-10-15 16:09:37 +0800

    Able to edit items that are already shown by double-clicking

--- anime-todo/src/components/TodoList.vue
+++ anime-todo/src/components/TodoList.vue
@@ -2,8 +2,9 @@
   <div>
     <input type="text" class="todo-input" placeholder="Watch an anime" v-model="newTodo" @keyup.enter="addTodo">
     <div v-for="(todo,index) in todos" :key="todo.id" class="todo-item">
-      <div>
-        {{ todo.title }}
+      <div class="todo-item-left">
+      <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-label">{{ todo.title }}</div>
+      <input v-else class="todo-item-edit" type="text" v-model="todo.title" @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" @keyup.escape="cancelEdit(todo)" v-focus>
       </div>
       <div class="remove-item" @click="removeTodo(index)">
         &times;
@@ -19,23 +20,34 @@ export default {
     return {
       newTodo: '',
       idForTodo: 3,
+      beforeEditCache: '',
       todos: [
         {
           'id': 1,
           'title': 'Yuru Yuri',
           'completed': false,
+          'editing': false,
         },
         {
           'id': 2,
           'title': 'Seitokai Yakuindomo',
           'completed': false,
+          'editing': false,
         },
         {
           'id': 3,
           'title': 'Joshiraku',
           'completed': false,
+          'editing': false,
         }
       ]
+    }
+  },
+  directives: {
+    focus: {
+      inserted: function(el) {
+        el.focus()
+      }
     }
   },
   methods: {
@@ -50,6 +62,20 @@ export default {
       })
       this.newTodo = ""
       this.idForTodo++
+    },
+    editTodo(todo) {
+      this.beforeEditCache = todo.title
+      todo.editing = true
+    },
+    doneEdit(todo) {
+      if (todo.title.trim().length == 0) {
+        todo.title = this.beforeEditCache
+      }
+      todo.editing = false
+    },
+    cancelEdit(todo) {
+      todo.title = this.beforeEditCache
+      todo.editing = false
     },
     removeTodo(index) {
       this.todos.splice(index, 1)
@@ -80,6 +106,27 @@ export default {
     margin-left: 14px;
     &:hover {
       color: black;
+    }
+  }
+  .todo-item-left {
+    display: flex;
+    align-items: center;
+  }
+  .todo-item-label {
+    padding: 10px;
+    border: 1px solid white;
+    margin-left: 12px;
+  }
+  .todo-item-edit {
+    font-size: 24px;
+    color: #2c3e50;
+    margin-left: 12px;
+    width: 100px;
+    padding: 10px;
+    border: 1px solid #ccc;
+    font-family: 'Avenir', Arial, Helvetica, sans-serif;
+    &:focus {
+      outline: none;
     }
   }
 </style>
